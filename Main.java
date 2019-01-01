6c7ed97f6e2d2d448cfa99a229ae43d87e3da082
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-01-01 22:53:44 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.tutorial;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    //my first program
-        System.out.println("Hello Winston! How's it going?");
-    }
-}
