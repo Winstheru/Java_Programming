d27dabb73e9e9ae9a486b9a5d7d3f003cf7a1971
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-01-01 22:34:06 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //my first program
+        System.out.println("Hello Winston! How's it going?");
+    }
+}
