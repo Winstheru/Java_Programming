931947140664b865a1601c4b2b4e5c1fe1796f01
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-01-01 22:53:18 +0700

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,1 +1,10 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //my first program
+        System.out.println("Hello Winston! How's it going?");
+    }
+}
 
